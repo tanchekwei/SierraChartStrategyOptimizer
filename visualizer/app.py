f84ca2579eb7316c0d8f134acb1ac6731f840750
tanchekwei <weiwei1@live.com.my>
2025-08-31 22:15:54 +0800

    Update

--- visualizer/app.py
+++ visualizer/app.py
@@ -1,5 +1,5 @@
-// Copyright (c) 2025 Chek Wei Tan
-// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+# Copyright (c) 2025 Chek Wei Tan
+# Licensed under the MIT License. See LICENSE file in the project root for full license information.
 
 import streamlit as st
 import pandas as pd
